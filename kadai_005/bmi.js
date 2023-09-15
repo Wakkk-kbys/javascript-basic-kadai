13a6d1633c36c19bb13c767818b294dc335e4a72
Wakkk-kbys <141903174+Wakkk-kbys@users.noreply.github.com>
2023-09-15 14:07:43 +0900

    kadai_005修正

---
+++ kadai_005/bmi.js
@@ -0,0 +1,11 @@
+// 変数の宣言・値の代入：体重
+let userWeight = 68;
+
+// 変数の宣言・値の代入：身長
+let userHeight = 1.7;
+
+// 体重(kg)]÷[身長(m)×身長(m)]
+let userBmi = userWeight/(userHeight * userHeight);
+
+//  コンソールへの出力
+console.log(userBmi);
